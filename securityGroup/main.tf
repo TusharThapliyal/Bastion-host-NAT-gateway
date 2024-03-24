a49d53459ebb17c28e929a8f1b99f656c0b34d74
Tushar Thapliyal <tusharthapliyal8979@gmail.com>
2024-03-24 06:34:16 +0530

    Update main.tf

--- securityGroup/main.tf
+++ securityGroup/main.tf
@@ -6,21 +6,21 @@ resource "aws_security_group" "publicSG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["49.36.219.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["49.36.219.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["49.36.219.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
